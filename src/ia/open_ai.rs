8d3ea09306417d7265e129def09e374992c75bfe
luedu1103 <luis.calle3@unmsm.edu.pe>
2024-05-22 17:40:41 -0500

    mayor changes

--- src/ia/open_ai.rs
+++ src/ia/open_ai.rs
@@ -34,41 +34,64 @@ pub mod chat_ai {
         Ok(string.clone())
     }
 
-    pub fn conversation(api_token: String, message: String) -> Result<String, Box<dyn std::error::Error>> {
+    // #[derive(Clone)]
+    pub struct ChatSession {
+        pub client: Client,
+        pub assistant_id: String,
+        pub thread_id: String,
+        // pub message: String,
+    }
+
+    impl ChatSession {
+        pub fn create_conversation(api_token: String) -> Result<Self, Box<dyn std::error::Error>> {
+            let client = Client::new(api_token);
+
+            let mut tools = HashMap::new();
+            tools.insert("type".to_string(), "code_interpreter".to_string());
+
+            let req = AssistantRequest::new(GPT3_5_TURBO.to_string());
+            let req = req
+                .clone()
+                .description("eres un maestro de chino y japonés".to_string());
+            let req = req.clone().instructions("Eres un maestro de chino y japonés, responde en el idioma en que se te pida. Siempre intenta guiar al usuario.".to_string());
+            let req = req.clone().tools(vec![tools]);
+            println!("{:?}", req);
+
+            let result = client.create_assistant(req)?;
+            let assistant_id = result.id.clone();
+            // println!("{:?}", result.id);
+
+            let thread_req = CreateThreadRequest::new();
+            let thread_result = client.create_thread(thread_req)?;
+            // println!("{:?}", thread_result.id.clone());
+
+            Ok(ChatSession {
+                client: client,
+                assistant_id : assistant_id,
+                thread_id : thread_result.id.clone(),
+                // message: string
+            })
+        }
+
+    }
+
+    pub fn talk(assistan_id: String, thread_id: String, message: String, api_token: String) -> Result<String, Box<dyn std::error::Error>>{
         let client = Client::new(api_token);
-    
-        let mut tools = HashMap::new();
-        tools.insert("type".to_string(), "code_interpreter".to_string());
-    
-        let req = AssistantRequest::new(GPT3_5_TURBO.to_string());
-        let req = req
-            .clone()
-            .description("eres un maestro de chino y japonés".to_string());
-        let req = req.clone().instructions("Eres un maestro de chino y japonés, responde en el idioma en que se te pida. Siempre intenta guiar al usuario.".to_string());
-        let req = req.clone().tools(vec![tools]);
-        println!("{:?}", req);
-    
-        let result = client.create_assistant(req)?;
-        println!("{:?}", result.id);
-    
-        let thread_req = CreateThreadRequest::new();
-        let thread_result = client.create_thread(thread_req)?;
-        println!("{:?}", thread_result.id.clone());
-    
+
         let message_req = CreateMessageRequest::new(
             MessageRole::user,
             message, // se le manda el mensaje
         );
-    
-        let message_result = client.create_message(thread_result.id.clone(), message_req)?;
+
+        let message_result = client.create_message(thread_id.clone(), message_req)?;
         println!("{:?}", message_result.id.clone());
-        
-        let run_req = CreateRunRequest::new(result.id);
-        let run_result = client.create_run(thread_result.id.clone(), run_req)?;
-        
+
+        let run_req = CreateRunRequest::new(assistan_id);
+        let run_result = client.create_run(thread_id.clone(), run_req)?;
+
         loop {
             let run_result = client
-            .retrieve_run(thread_result.id.clone(), run_result.id.clone())
+            .retrieve_run(thread_id.clone(), run_result.id.clone())
             .unwrap();
         if run_result.status == "completed" {
             break;
@@ -77,9 +100,9 @@ pub mod chat_ai {
                 std::thread::sleep(std::time::Duration::from_secs(1));
             }
         }
-        
+
         let mut string = String::new();
-        let list_message_result = client.list_messages(thread_result.id.clone()).unwrap();
+        let list_message_result = client.list_messages(thread_id.clone()).unwrap();
         for data in list_message_result.data {
             for content in data.content {
                 println!(
@@ -90,7 +113,7 @@ pub mod chat_ai {
                 string.push_str(" ");
             }
         }
-        
+
         string = string.trim_end().to_string();
 
         Ok(string)
--- src/main.rs
+++ src/main.rs
@@ -14,46 +14,57 @@ struct MyState {
 #[macro_use]
 extern crate rocket;
 
-const GPTHOLA: Origin<'static> = uri!("/long-laoshi");
+const GPTHOLA: Origin<'static> = uri!("/");
 
 #[get("/")]
 fn index() -> Redirect {
     let msg: Option<&str> = None;
-    Redirect::to(uri!(GPTHOLA, google_keep_desktop_api(msg)))
+    Redirect::to(uri!(GPTHOLA, longlaoshi_main_page(msg)))
 }
 
 // endpoint
 // /tauri-releases/google-keep-desktop&win64&1.18.0?msg=""
 #[get("/longlaoshi?<msg>")]
-fn google_keep_desktop_api(msg: Option<&str>) -> Result<Value, Status> {
+fn longlaoshi_main_page(msg: Option<&str>) -> Result<Value, Status> {
     // Status::NoContent
+    let mut string = "";
     if let Some(msg) = msg {
-        println!("{msg}");
-        return  Err(Status::NoContent);
+        string = msg;
     }
 
     Ok(json!({
-        "notes": "it works",
+        "notes": string
     }))
 }
 
 #[get("/chat_with_ai?<msg>")]
 fn open_ai_chat(state: &State<MyState>, msg: String) -> Result<Value, Status> {
-    let string = chat_ai::chat(msg, state.secret.to_owned()).unwrap_or(Some("un error parece".to_string()));
+    let string = chat_ai::chat(msg, state.secret.to_owned()).unwrap_or(Some("Esto es un error".to_string()));
 
     Ok(json!({
         "notes": string
     }))
 }
 
-#[get("/conversation?<msg>")]
-fn open_ai_convo(state: &State<MyState>, msg: String) -> Result<Value, Status> {
-    let string = chat_ai::conversation(state.secret.to_owned(), msg).unwrap_or("un error parece".to_string());
+#[get("/create-conversation")]
+fn open_ai_create_conversation(state: &State<MyState>) -> Result<Value, Status> {
+    let chat_session = chat_ai::ChatSession::create_conversation(state.secret.to_owned()).unwrap();
     // println!("{:?}", chat_ai::conversation(state.secret.to_owned(), msg));
     Ok(json!({
-        "notes": string
+        "id": chat_session.assistant_id,
+        "thread": chat_session.thread_id,
+        "message": "its working",
     }))
 }
+
+#[get("/chat-with-your-own-assistant/<id>/<thread>/<msg>")]
+fn open_ai_chat_with_your_assistant(id: String, thread: String, msg: String, state: &State<MyState>) -> Result<Value, Status> {
+    let response = chat_ai::talk(id, thread, msg, state.secret.to_owned()).unwrap();
+    Ok(json!({
+        "response": response
+    }))
+}
+
 // #[launch]
 // fn rocket() -> _ {
 //     rocket::build()
@@ -74,9 +85,10 @@ async fn main(#[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore)
 
     let rocket = rocket::build()
         .mount("/", routes![index])
-        .mount(GPTHOLA, routes![google_keep_desktop_api])
+        .mount("/", routes![longlaoshi_main_page])
         .mount(GPTHOLA, routes![open_ai_chat])
-        .mount("/", routes![open_ai_convo])
+        .mount("/", routes![open_ai_create_conversation])
+        .mount("/", routes![open_ai_chat_with_your_assistant])
         .manage(state);
 
     Ok(rocket.into())
