833e4d690620e78e0cd72df67508ac6f20945269
Luis Calle <luedu1103@gmail.com>
2024-06-08 15:43:54 -0500

    Voice route and install fixing

--- static/index.js
+++ static/index.js
@@ -2,7 +2,7 @@ const postButton = document.getElementById('postButton');
 // const loadingIndicator = document.getElementById('loadingIndicator');
 
 postButton.addEventListener('click', async () => {
-    const url = 'https://longlaoshi-server.shuttleapp.rs/download-apk';
+    const url = 'https://longlaoshi-server.shuttleapp.rs/longlaoshi/download-apk';
             const a = document.createElement('a');
             a.href = url;
             a.download = ''; // Let the server dictate the filename
